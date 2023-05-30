2228069d0b3275c10ab565bf8d605d72608efec8
uphong157 <uphong157@gmail.com>
2023-05-29 17:30:12 -0700

    Complete exercise 3.15

--- index.js
+++ index.js
@@ -51,10 +51,10 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(p => p.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
 })
 
 app.post('/api/persons', (request, response) => {
